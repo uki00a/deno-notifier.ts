0fa8c4c5d5781a92771c7edd571ce4935cd4db17
uki00a <uki00a@gmail.com>
2021-09-11 04:02:30 +0900

    chore: Add @deno-types to td.js

--- test_deps.ts
+++ test_deps.ts
@@ -3,4 +3,5 @@ export {
   assertThrowsAsync,
 } from "https://deno.land/std@0.106.0/testing/asserts.ts";
 
+// @deno-types="https://unpkg.com/testdouble@3.16.1/index.d.ts"
 export * as td from "https://cdn.skypack.dev/testdouble@3.16.1/dist/testdouble.js";
