b69b8fb922bb3382a5635ee82a7b727444717c89
uki00a <uki00a@gmail.com>
2021-09-11 03:32:28 +0900

    chore: Bump Deno to v1.13

--- test_deps.ts
+++ test_deps.ts
@@ -1,7 +1,7 @@
 export {
   assertEquals,
   assertThrowsAsync,
-} from "https://deno.land/std@0.97.0/testing/asserts.ts";
+} from "https://deno.land/std@0.106.0/testing/asserts.ts";
 
 // @deno-types="https://esm.sh/testdouble@3.16.1/index.d.ts"
 export * as td from "https://esm.sh/testdouble@3.16.1/dist/testdouble.js";
