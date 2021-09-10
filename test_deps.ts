a8896fd199b7704d0f62408041ed1890bcade476
uki00a <uki00a@gmail.com>
2021-09-11 03:59:13 +0900

    chore: Remove dts

--- test_deps.ts
+++ test_deps.ts
@@ -3,4 +3,4 @@ export {
   assertThrowsAsync,
 } from "https://deno.land/std@0.106.0/testing/asserts.ts";
 
-export * as td from "https://cdn.skypack.dev/testdouble@3.16.1/dist/testdouble.js?dts";
+export * as td from "https://cdn.skypack.dev/testdouble@3.16.1/dist/testdouble.js";
