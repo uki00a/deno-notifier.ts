00a79bc22a81d6c62589dfe6bddfee92289c538b
uki00a <uki00a@gmail.com>
2021-09-11 03:35:58 +0900

    fix: Update @deno-types for td.js

--- test_deps.ts
+++ test_deps.ts
@@ -3,5 +3,5 @@ export {
   assertThrowsAsync,
 } from "https://deno.land/std@0.106.0/testing/asserts.ts";
 
-// @deno-types="https://esm.sh/testdouble@3.16.1/index.d.ts"
+// @deno-types="https://unpkg.com/testdouble@3.16.1/index.d.ts"
 export * as td from "https://esm.sh/testdouble@3.16.1/dist/testdouble.js";
