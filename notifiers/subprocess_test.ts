9315f974c016d34fb1ded1a25575a8124a3e42a4
Yuki Tanaka <uki00a@gmail.com>
2021-09-12 19:50:17 +0900

    test: Add testcase for SubprocessNotifier (#7)

---
+++ notifiers/subprocess_test.ts
@@ -0,0 +1,28 @@
+import { assertRejects } from "../test_deps.ts";
+import { SubprocessNotifier } from "./subprocess.ts";
+import type { Notification } from "./notifier.ts";
+
+class TestingNotifier extends SubprocessNotifier {
+  buildCmd(notification: Notification): string[] {
+    const payload = JSON.stringify(notification);
+    return [Deno.execPath(), "eval", `throw new Error(\`${payload}\`)`];
+  }
+}
+
+Deno.test({
+  name: "SubprocessNotifier",
+  ignore: Deno.build.os !== "linux",
+  async fn() {
+    const notifier = new TestingNotifier();
+    const notification = {
+      title: "Hello",
+      message: "World",
+    };
+    const expectedMessage = JSON.stringify(notification);
+    await assertRejects(
+      () => notifier.notify(notification),
+      Error,
+      expectedMessage,
+    );
+  },
+});
--- test_deps.ts
+++ test_deps.ts
@@ -1,6 +1,6 @@
 export {
   assertEquals,
-  assertThrowsAsync,
+  assertRejects,
 } from "https://deno.land/std@0.106.0/testing/asserts.ts";
 
 // @deno-types="https://esm.sh/testdouble@3.16.1/index.d.ts"
