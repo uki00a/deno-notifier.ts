50bc9ed7ad0c15da8338899667fcfd719266acd0
uki00a <uki00a@gmail.com>
2021-09-12 19:32:46 +0900

    chore: Add deno-types

--- test_deps.ts
+++ test_deps.ts
@@ -3,4 +3,5 @@ export {
   assertThrowsAsync,
 } from "https://deno.land/std@0.106.0/testing/asserts.ts";
 
-export * as td from "https://esm.sh/testdouble@3.16.1/dist/testdouble.js";
+// @deno-types="https://esm.sh/testdouble@3.16.1/index.d.ts"
+export * as td from "https://esm.sh/testdouble@3.16.1/dist/testdouble.js?no-check";
