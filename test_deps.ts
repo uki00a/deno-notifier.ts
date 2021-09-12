e2a6158b253df46c58f22feb3eeeaa7e9ca632f6
uki00a <uki00a@gmail.com>
2021-09-12 19:35:30 +0900

    chore: Update

--- test_deps.ts
+++ test_deps.ts
@@ -4,4 +4,4 @@ export {
 } from "https://deno.land/std@0.106.0/testing/asserts.ts";
 
 // @deno-types="https://esm.sh/testdouble@3.16.1/index.d.ts"
-export * as td from "https://esm.sh/testdouble@3.16.1/dist/testdouble.js?no-check";
+export { default as td } from "https://esm.sh/testdouble@3.16.1/dist/testdouble.js?no-check";
