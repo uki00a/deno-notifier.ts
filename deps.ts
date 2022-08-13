cfe7e4af433dd757cd9b3f829cb45507ba13deb4
uki00a <uki00a@gmail.com>
2022-08-13 15:07:50 +0900

    deps: `deno@v1.24.3`, `deno_std@v0.152.0`

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
-export { readAll } from "https://deno.land/std@0.106.0/io/util.ts";
-export { deferred } from "https://deno.land/std@0.106.0/async/deferred.ts";
-export type { Deferred } from "https://deno.land/std@0.106.0/async/deferred.ts";
+export { readAll } from "https://deno.land/std@0.152.0/io/util.ts";
+export { deferred } from "https://deno.land/std@0.152.0/async/deferred.ts";
+export type { Deferred } from "https://deno.land/std@0.152.0/async/deferred.ts";
