94b8de76afe81bff829ba86975dd36ee84d79f96
uki00a <uki00a@gmail.com>
2023-03-07 05:31:16 +0900

    fix: remove `target=denonext`

--- test_deps.ts
+++ test_deps.ts
@@ -4,4 +4,4 @@ export {
 } from "https://deno.land/std@0.178.0/testing/asserts.ts";
 
 // @deno-types="https://esm.sh/testdouble@3.16.1/index.d.ts"
-export * as td from "https://esm.sh/testdouble@3.16.1/dist/testdouble.js?no-check&target=denonext";
+export * as td from "https://esm.sh/testdouble@3.16.1/dist/testdouble.js?no-check";
