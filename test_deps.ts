1fe7e54e220a0b483d8fcc1f47965266ea7f8c80
uki00a <uki00a@gmail.com>
2023-05-17 05:33:51 +0900

    deps: `deno@v1.33.3`, `deno_std@v0.187.0`

--- test_deps.ts
+++ test_deps.ts
@@ -1,6 +1,6 @@
 export {
   assertEquals,
   assertRejects,
-} from "https://deno.land/std@0.178.0/testing/asserts.ts";
+} from "https://deno.land/std@0.187.0/testing/asserts.ts";
 
 export * as td from "npm:testdouble@3.16.1";
